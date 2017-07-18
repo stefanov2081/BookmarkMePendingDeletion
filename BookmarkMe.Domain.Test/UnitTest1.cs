01a1d3416459156f86cd65d36a7c019af7e89938
Vladimir Stefanov <Vladimir Stefanov@VLADIMIRSTEFANO>
2017-07-18 11:53:25 +0300

    Add project files.

---
+++ BookmarkMe.Domain.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookmarkMe.Domain.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
